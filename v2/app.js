d7e41cb34991d3feb540e1b9a2822632727fc82f
Abisai Rodriguez <abisai.rodriguez13@gmail.com>
2020-01-15 22:36:10 -0800

    Campground Show page created

--- v2/app.js
+++ v2/app.js
@@ -10,14 +10,17 @@ app.set("view engine", "ejs");
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create({
 //     name: "Eagle Point", 
-//     image: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+//     image: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+//     description: "This is an eagle point, no bathrooms. No water. Beautiful eagle point"
+
 // }, function(err, campground){
 //     if(err){
 //         console.log(err);
@@ -33,6 +36,7 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+//INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res){
     // Get all campgrounds from DB
     Campground.find({}, function(err, allCampgrounds){
@@ -40,17 +44,19 @@ app.get("/campgrounds", function(req, res){
             console.log(err);
         }
         else{
-            res.render("campgrounds", {campgrounds: allCampgrounds});
+            res.render("index", {campgrounds: allCampgrounds});
         }
     });
     //res.render("campgrounds", {campgrounds: campgrounds});
 });
 
+//CREATE - add new campgrounds to DB
 app.post("/campgrounds", function(req, res){
     //get data from form & add to campgrounds array
     const name = req.body.name;
     const image = req.body.image;
-    const newCampground = {name: name, image: image};
+    const desc = req.body.description;
+    const newCampground = {name: name, image: image, description: desc};
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
@@ -63,8 +69,23 @@ app.post("/campgrounds", function(req, res){
     });
 });
 
+//NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
     res.render("new.ejs");
+});
+
+//SHOW - more info about campground
+app.get("/campgrounds/:id", function(req, res){
+    //Find the campground with the provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }
+        else{
+            //render show template with that campground
+            res.render("show", {campground: foundCampground });
+        }
+    });
 });
 
 app.listen(3000);
