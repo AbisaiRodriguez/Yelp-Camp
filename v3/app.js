676d7908295495803bafa56ffbd941f8ad3499d1
Abisai Rodriguez <abisai.rodriguez13@gmail.com>
2020-04-12 15:50:55 -0700

    added the comments to the show template page

--- v3/app.js
+++ v3/app.js
@@ -5,26 +5,11 @@ const express    = require("express"),
       Campground = require("./models/campground"),
       seedDB     = require("./seeds");
 
-seedDB();      
+     
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useUnifiedTopology: true, useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-// Campground.create({
-//     name: "Eagle Point", 
-//     image: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-//     description: "This is an eagle point, no bathrooms. No water. Beautiful eagle point"
-
-// }, function(err, campground){
-//     if(err){
-//         console.log(err);
-//     }
-//     else{
-//         console.log("newly created campground");
-//         console.log(campground);
-//     }
-// });
-
+seedDB(); 
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -71,11 +56,12 @@ app.get("/campgrounds/new", function(req, res){
 //SHOW - more info about campground
 app.get("/campgrounds/:id", function(req, res){
     //Find the campground with the provided ID
-    Campground.findById(req.params.id, function(err, foundCampground){
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
         if(err){
             console.log(err);
         }
         else{
+            console.log(foundCampground);
             //render show template with that campground
             res.render("show", {campground: foundCampground });
         }
